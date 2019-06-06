715ea001fc4130f9852dc3883cbf2ec26fd60873
jdsoteldo <jdsoteldo00@gmail.com>
2019-06-06 17:30:36 -0500

    deleted email from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_141013) do
+ActiveRecord::Schema.define(version: 2019_06_06_222345) do
 
   create_table "clicks", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_05_24_141013) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "last_name"
-    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
