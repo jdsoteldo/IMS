f51cb7428e0d7e675af49056f0a63820657eebb3
jdsoteldo <jdsoteldo00@gmail.com>
2019-06-12 18:57:37 -0500

    navbar started

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :tables
+  after_create :assign_default_role
   rolify
+
+  def assign_default_role
+    self.add_role(:newuser) if self.roles.blank?
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
